4dafd4abb70ebba91bf279e66ee899dd5b488ebc
surprise3465 <spirit3465@sina.com>
2017-09-08 17:45:34 +0800

    20170908

---
+++ PingTestTool/Form1.Designer.cs
@@ -0,0 +1,327 @@
+﻿namespace PingTestTool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button_begin = new System.Windows.Forms.Button();
+            this.button_clear = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.textBox_trytimes = new System.Windows.Forms.TextBox();
+            this.textBox_timeout = new System.Windows.Forms.TextBox();
+            this.textBox_packetsize = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox_interval = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Configure File";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(122, 14);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(376, 21);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(513, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(73, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(-2, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(626, 42);
+            this.panel1.TabIndex = 3;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6,
+            this.Column7});
+            this.dataGridView1.Enabled = false;
+            this.dataGridView1.GridColor = System.Drawing.Color.Silver;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 41);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(612, 251);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Column1
+            // 
+            this.Column1.FillWeight = 50F;
+            this.Column1.HeaderText = "Name";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            this.Column1.Width = 65;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "IpAddr";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            this.Column2.Width = 120;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Status";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            this.Column3.Width = 90;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Time (ms)";
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            this.Column4.Width = 90;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Speed (Mb/s)";
+            this.Column5.Name = "Column5";
+            this.Column5.ReadOnly = true;
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "Error";
+            this.Column6.Name = "Column6";
+            this.Column6.ReadOnly = true;
+            this.Column6.Width = 70;
+            // 
+            // Column7
+            // 
+            this.Column7.HeaderText = "Total";
+            this.Column7.Name = "Column7";
+            this.Column7.ReadOnly = true;
+            this.Column7.Width = 70;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button_begin
+            // 
+            this.button_begin.Font = new System.Drawing.Font("宋体", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button_begin.Location = new System.Drawing.Point(511, 309);
+            this.button_begin.Name = "button_begin";
+            this.button_begin.Size = new System.Drawing.Size(90, 40);
+            this.button_begin.TabIndex = 5;
+            this.button_begin.Text = "Begin";
+            this.button_begin.UseVisualStyleBackColor = true;
+            this.button_begin.Click += new System.EventHandler(this.button_begin_Click);
+            // 
+            // button_clear
+            // 
+            this.button_clear.Location = new System.Drawing.Point(511, 368);
+            this.button_clear.Name = "button_clear";
+            this.button_clear.Size = new System.Drawing.Size(90, 40);
+            this.button_clear.TabIndex = 6;
+            this.button_clear.Text = "ClearData";
+            this.button_clear.UseVisualStyleBackColor = true;
+            this.button_clear.Click += new System.EventHandler(this.button_clear_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 309);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "TryTimes";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(46, 337);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 12);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Timeout";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(234, 312);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 12);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Packetsize";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(233, 338);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(144, 16);
+            this.checkBox1.TabIndex = 10;
+            this.checkBox1.Text = "options.DontFragment";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // textBox_trytimes
+            // 
+            this.textBox_trytimes.Location = new System.Drawing.Point(98, 306);
+            this.textBox_trytimes.Name = "textBox_trytimes";
+            this.textBox_trytimes.Size = new System.Drawing.Size(100, 21);
+            this.textBox_trytimes.TabIndex = 11;
+            // 
+            // textBox_timeout
+            // 
+            this.textBox_timeout.Location = new System.Drawing.Point(98, 334);
+            this.textBox_timeout.Name = "textBox_timeout";
+            this.textBox_timeout.Size = new System.Drawing.Size(100, 21);
+            this.textBox_timeout.TabIndex = 12;
+            // 
+            // textBox_packetsize
+            // 
+            this.textBox_packetsize.Location = new System.Drawing.Point(304, 309);
+            this.textBox_packetsize.Name = "textBox_packetsize";
+            this.textBox_packetsize.Size = new System.Drawing.Size(100, 21);
+            this.textBox_packetsize.TabIndex = 13;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(40, 368);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 12);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Interval";
+            // 
+            // textBox_interval
+            // 
+            this.textBox_interval.Location = new System.Drawing.Point(98, 365);
+            this.textBox_interval.Name = "textBox_interval";
+            this.textBox_interval.Size = new System.Drawing.Size(100, 21);
+            this.textBox_interval.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(625, 442);
+            this.Controls.Add(this.textBox_interval);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox_packetsize);
+            this.Controls.Add(this.textBox_timeout);
+            this.Controls.Add(this.textBox_trytimes);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button_clear);
+            this.Controls.Add(this.button_begin);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "PingTester";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button_begin;
+        private System.Windows.Forms.Button button_clear;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox textBox_trytimes;
+        private System.Windows.Forms.TextBox textBox_timeout;
+        private System.Windows.Forms.TextBox textBox_packetsize;
+        private System.Windows.Forms.BindingSource pingResultEntryBindingSource;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox_interval;
+    }
+}
+
---
+++ PingTestTool/Form1.cs
@@ -0,0 +1,340 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Threading;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace PingTestTool
+{
+    public delegate void UpdateDelegate(string info);
+    public partial class Form1 : Form
+    {
+        private string path = AppDomain.CurrentDomain.BaseDirectory;
+        private UpdateDelegate updateUI = null;
+
+        public class Device
+        {
+            public string Ip;
+            public string Name;
+            public PingResult pingResult;
+            public Device(string ip, string name)
+            {
+                Ip = ip;
+                Name = name;
+                pingResult = new PingResult();
+            }
+        }
+
+        private List<Device> deviceList = new List<Device>();
+        private Thread[] thread;
+        private Thread wait;
+        private int time_out;
+        private int Package_size;
+        private int try_times;
+        private int interval;
+        public enum PingResultEntryStatus
+        {
+            Success,
+            GenericFailureSeeReplyStatus,
+            PingAbortedForHighNetworkUsage,
+            PingAbortedUnableToGetNetworkUsage,
+            ExceptionRaisedDuringPing
+        };
+
+        public class PingResultEntry
+        {
+            public double? Rtt { get; private set; }
+            public IPStatus? IpStatus { get; private set; }
+            public PingResultEntryStatus Status { get; private set; }
+            public DateTime Time { get; private set; }
+
+            public PingResultEntry(double? rtt, IPStatus? ipStatus, PingResultEntryStatus status, DateTime time)
+            {
+                IpStatus = ipStatus;
+                Rtt = rtt;
+                Status = status;
+                Time = time;
+            }
+        }
+
+        public class PingResult
+        {
+            public List<PingResultEntry> results;
+            public int? err_num;
+            public int? total_num;
+            private double? avg;
+            private double? speed;
+            public PingResult()
+            {
+                results = new List<PingResultEntry>();
+                err_num = 0;
+                total_num = 0;
+            }
+
+            public void addPingResultEntry(PingResultEntry newEntry)
+            {
+                // Adding a new entry
+                results.Add(newEntry);
+                // Reset any previously calculated stats (they where probably already null but who knows...)
+                avg = null;
+                speed = null;
+            }
+
+            public double? getSpeed(int length)
+            {
+                // Calculating speed if not yet calculated
+                if (avg <= 0) return 1024.0F;
+                return (double)length/avg*1000/1024;
+            }
+
+            public double? getAvg()
+            {
+                // Calculating avg if not yet calculated
+                if (avg == null && results.Count(res => res.Status == PingResultEntryStatus.Success) > 0)
+                {
+                    avg = new double?(0);
+                    int cont = 0;
+                    foreach (PingResultEntry e in results.Where(x => x.Status == PingResultEntryStatus.Success))
+                    {
+                        cont++;
+                        avg += e.Rtt;
+                    }
+                    if (cont > 0)
+                    {
+                        avg = avg / cont;
+                    }
+                }
+                return avg;
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.Text = path + "ConfigData.xml";
+
+            textBox_timeout.Text = "300";
+            textBox_trytimes.Text = "200";
+            textBox_packetsize.Text = "4096";
+            checkBox1.Checked = false;
+            textBox_interval.Text = "100";
+            LoadXmlFile(textBox1.Text);
+
+            updateUI = UpdateList;  
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (DialogResult.OK == openFileDialog1.ShowDialog())
+            {
+                textBox1.Text = openFileDialog1.FileName;
+                LoadXmlFile(textBox1.Text);
+            }
+        }
+
+        private void LoadXmlFile(string fileName)
+        {
+            try
+            {
+                XmlDocument doc = new XmlDocument();
+                doc.Load(fileName);
+                deviceList.Clear();
+                XmlNodeList list = doc.SelectNodes("/Root/Project/DeviceList/Device");
+                foreach (XmlNode n in list)
+                {
+                    deviceList.Add(new Device(n.Attributes["ip"].Value, n.Attributes["name"].Value));                   
+                }
+                foreach (var dev  in deviceList)
+                {
+                    int i = dataGridView1.Rows.Add();
+                    dataGridView1.Rows[i].Cells[0].Value = dev.Name;
+                    dataGridView1.Rows[i].Cells[1].Value = dev.Ip;
+                }
+                dataGridView1.ClearSelection();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button_begin_Click(object sender, EventArgs e)
+        {
+            if (deviceList.Count == 0)
+            {
+                MessageBox.Show("There is no device！");
+                return;
+            }
+
+            if (button_begin.Text == "Stop")
+            {
+                wait?.Abort();
+                wait = null;
+                for (var i = 0; i < thread.Length; i++)
+                {
+                    if (thread[i] == null || !thread[i].IsAlive) continue;
+                    thread[i].Abort();
+                    thread[i] = null;
+                }
+                button_begin.Text = "Begin";
+                textBox_packetsize.Enabled = true;
+                textBox_timeout.Enabled = true;
+                textBox_trytimes.Enabled = true;
+                checkBox1.Enabled = true;
+                button_clear.Enabled = true;
+                textBox_interval.Enabled = true;
+            }
+            else
+            {
+                button_begin.Text = "Stop";
+                textBox_packetsize.Enabled = false;
+                textBox_timeout.Enabled = false;
+                textBox_trytimes.Enabled = false;
+                textBox_interval.Enabled = false;
+                checkBox1.Enabled = false;
+                button_clear.Enabled = false;
+
+                Package_size = Convert.ToInt32(textBox_packetsize.Text);
+                time_out = Convert.ToInt32(textBox_timeout.Text);
+                try_times = Convert.ToInt32(textBox_trytimes.Text);
+                interval = Convert.ToInt32(textBox_interval.Text);
+                thread = new Thread[deviceList.Count];
+                for (var i = 0; i < deviceList.Count; i++)
+                {
+                    thread[i] = new Thread(Threadhandler);
+                    thread[i].Start(i);
+                }
+
+                wait= new Thread(waitforallthread);
+                wait.Start();
+            }
+        }
+
+        private void waitforallthread()
+        {
+            bool flag = true;
+            Thread.Sleep(2000);
+            while (flag)
+            {
+                flag = false;
+                foreach (var thr in thread.Where(thr => thr.IsAlive))
+                {
+                    Thread.Sleep(200);
+                    flag = true;
+                }
+            }
+
+            BeginInvoke(updateUI, "button");
+        }
+
+        private void Threadhandler(object index)
+        {           
+            Ping pingSender = new Ping();
+            PingOptions options = new PingOptions();
+            byte[] buffer = new byte[Package_size];
+            options.DontFragment = checkBox1.Checked;
+            for (var i = 0; i < try_times; i++)
+            {
+                Device dev = deviceList[(int)index];
+                try
+                {
+                    PingReply reply = pingSender.Send(dev.Ip, time_out, buffer, options);
+                    if (reply.Status == IPStatus.Success)
+                    {
+                        // All has gone well
+                        dev.pingResult.addPingResultEntry(new PingResultEntry(
+                            reply.RoundtripTime, reply.Status, PingResultEntryStatus.Success, System.DateTime.Now));
+
+                    }
+                    else
+                    {
+                        // Something went wrong, wrong but "expected"
+                        dev.pingResult.addPingResultEntry(new PingResultEntry(
+                            reply.RoundtripTime, reply.Status, PingResultEntryStatus.GenericFailureSeeReplyStatus,
+                            System.DateTime.Now));
+                        dev.pingResult.err_num++;
+                    }
+                    dev.pingResult.total_num++;
+                    dev.pingResult.getAvg();
+                    dev.pingResult.getSpeed(Package_size);
+                }
+                catch
+                {
+                    dev.pingResult.addPingResultEntry(new PingResultEntry(
+                                    null, null, PingResultEntryStatus.ExceptionRaisedDuringPing, System.DateTime.Now));
+                }
+
+                BeginInvoke(updateUI, "list"+index);
+
+                Thread.Sleep(interval);
+            }
+        }
+
+        private void UpdateList(string info)
+        {
+            if (info.Contains("list"))
+            {
+                int i = Convert.ToInt32(info.Remove(0,4));
+                Device dev = deviceList[i];
+                dataGridView1.Rows[i].Cells[0].Value = dev.Name;
+                dataGridView1.Rows[i].Cells[1].Value = dev.Ip;
+                var ipStatus = dev.pingResult.results.Last().IpStatus;
+                if (ipStatus != null)
+                    dataGridView1.Rows[i].Cells[2].Value = ipStatus.ToString();
+
+                var avg = dev.pingResult.getAvg();
+                if (avg != null)
+                    dataGridView1.Rows[i].Cells[3].Value = Math.Round((double) avg, 2).ToString();
+                var speed = dev.pingResult.getSpeed(Package_size);
+                if (speed != null)
+                    dataGridView1.Rows[i].Cells[4].Value = Math.Round((double)speed, 2).ToString();
+                dataGridView1.Rows[i].Cells[5].Value = dev.pingResult.err_num.ToString();
+                dataGridView1.Rows[i].Cells[6].Value = dev.pingResult.total_num.ToString();
+                
+                dataGridView1.Refresh();
+                dataGridView1.ClearSelection();
+            }
+            else if(info == "button")
+            {
+                button_begin.Text = "Begin";
+                textBox_packetsize.Enabled = true;
+                textBox_timeout.Enabled = true;
+                textBox_trytimes.Enabled = true;
+                checkBox1.Enabled = true;
+                button_clear.Enabled = true;
+                textBox_interval.Enabled = true;
+            }
+        }
+
+        private void button_clear_Click(object sender, EventArgs e)
+        {
+            foreach(var dev in deviceList)
+            {
+                dev.pingResult.results.Clear();
+                dev.pingResult.err_num = 0;
+                dev.pingResult.total_num = 0;
+            }
+            dataGridView1.ResetText();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.ClearSelection();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            wait?.Abort();
+            wait = null;
+            for (var i = 0; i < thread.Length; i++)
+            {
+                if (thread[i] == null || !thread[i].IsAlive) continue;
+                thread[i].Abort();
+                thread[i] = null;
+            }
+
+        }
+    }
+}
